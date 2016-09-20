504e6743ebf043e768918d77223ac9fcd6d5f519
FBisca <felipe15yo@gmail.com>
2016-09-20 15:33:21 -0300

    Correcting sample app

--- app/src/main/kotlin/com/ninenine/reactivelocation/sample/MainActivity.kt
+++ app/src/main/kotlin/com/ninenine/reactivelocation/sample/MainActivity.kt
@@ -1,26 +1,80 @@
 package com.ninenine.reactivelocation.sample
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.TextView
 import com.ninenine.reactivelocation.FusedLocationApiProvider
+import com.ninenine.reactivelocation.LocationConnectionException
 import com.ninenine.reactivelocation.LocationManager
+import rx.Subscription
 
 class MainActivity : AppCompatActivity() {
 
   val TAG = "Reactive Location"
+  val REQUEST_CODE_LOCATION_EXCEPTION = 1
+
+  val fusedLocationApi by lazy { FusedLocationApiProvider(this) }
+
+  var subscription: Subscription? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+  }
 
-    val fusedLocationApi = FusedLocationApiProvider(this)
-    fusedLocationApi.connect()
+  override fun onStart() {
+    super.onStart()
+    startListeningToLocation()
+  }
 
-    LocationManager(fusedLocationApi).connect().subscribe(
-        { location -> Log.d(TAG, "Location: ${location.latitude}, ${location.longitude}") },
-        { error -> Log.e(TAG, "Error: ${error.message}") },
-        { Log.d(TAG, "Completed") }
+  override fun onStop() {
+    super.onStop()
+    subscription?.unsubscribe()
+  }
+
+  fun startListeningToLocation() {
+    val manager = LocationManager(fusedLocationApi)
+    subscription = manager.streamForRequest().subscribe(
+        { location ->
+          val text = "Location: ${location.latitude}, ${location.longitude}"
+          val lastLocation = findViewById(R.id.lastLocation) as TextView
+          lastLocation.text = text
+
+          Log.d(TAG, text)
+        },
+        { error ->
+          if (error is LocationConnectionException && error.hasSolution()) {
+            error.startActivityForSolution(this, REQUEST_CODE_LOCATION_EXCEPTION)
+          }
+          Log.e(TAG, "Error: ${error.message}")
+        },
+        {
+          Log.d(TAG, "Completed")
+        }
     )
+  }
+
+  override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    if (requestCode == REQUEST_CODE_LOCATION_EXCEPTION) {
+      permissions.forEachIndexed { index, permission ->
+        if (permission.equals(Manifest.permission.ACCESS_FINE_LOCATION) && grantResults[index] == PackageManager.PERMISSION_GRANTED) {
+          startListeningToLocation()
+        }
+      }
+
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (requestCode == REQUEST_CODE_LOCATION_EXCEPTION && resultCode == Activity.RESULT_OK) {
+      startListeningToLocation()
+    }
   }
 }
